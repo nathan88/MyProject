fbddc1c1bdf10d2b359beea75fbdfdca74d5a414
Kat Choi <kchoi@seasoft.com>
2018-07-21 13:05:16 -0400

    init project

---
+++ myapp-backend/src/com/kat/myapp/backend/database/DataSourceManager.java
@@ -0,0 +1,42 @@
+package com.kat.myapp.backend.database;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import org.apache.commons.dbcp2.BasicDataSource;
+
+public class DataSourceManager {
+
+	private static DataSourceManager _instance = new DataSourceManager();
+	private BasicDataSource dataSource;
+
+	private DataSourceManager() {
+		dataSource = getDataSource();
+	}
+
+	public static DataSourceManager getInstance() {
+		return _instance;
+	}
+
+	private BasicDataSource getDataSource() {
+
+		if (dataSource == null) {
+			BasicDataSource ds = new BasicDataSource();
+			ds.setUrl("jdbc:mysql://localhost/myapp");
+			ds.setUsername("root");
+			ds.setPassword("toor");
+			ds.setMinIdle(5);
+			ds.setMaxIdle(10);
+			ds.setMaxOpenPreparedStatements(100);
+
+			dataSource = ds;
+		}
+		return dataSource;
+	}
+
+	public Connection getConnection() throws SQLException {
+		Connection connection = dataSource.getConnection();
+		return connection;
+
+	}
+
+}
